a5fd472eb2e36adc912419812f1f3d498fa74785
Rahul NS <rahul9380767545@gmail.com>
2025-03-09 23:50:44 +0530

    UI: Dark mode is now the primary theme

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,16 @@
 import { PropsWithChildren } from "react";
+import Header from "./header";
 
 const Layout = ({children}: PropsWithChildren) => {
     return (
         <div className="bg-gradient-to-br from background to-muted">
-            header
+            <Header />
             <main className="min-h-screen container mx-auto px-4 py-8">
                 {children}
             </main>
             <footer className="border-t backdrop-blur">
-                <div className="container mx-auto px-4 py-12 text-center text-gray-400">
+                <div className="container mx-auto px-4 py-12
+                text-center text-gray-400 supports-[backdrop-filter]:bg-background/60">
                     <p>Made by Rahul</p>
                 </div>
             </footer>
---
+++ src/pages/city-page.tsx
@@ -0,0 +1,5 @@
+export default function CityPage() {
+    return <div>
+        CityPage
+    </div>
+}
