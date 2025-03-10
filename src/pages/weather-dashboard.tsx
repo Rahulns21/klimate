3a6808ba885c99ce4484d9d3336dc806529460a0
Rahul NS <rahul9380767545@gmail.com>
2025-03-10 15:20:35 +0530

    feat: add dark and light theme toggle

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
+
 export default function WeatherDashboard() {
     return <div>
-        WeatherPage
+        {/* favorite cities */}
+        <div className="flex items-center justify-between">
+            <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+            <Button variant={'outline'} 
+            size={'icon'} 
+            // onClick={handleRefresh}
+            // disabled={}
+            >
+                <RefreshCcw />
+            </Button>
+        </div>
+
+        {/* current and hourly weather */}
     </div>
 }
