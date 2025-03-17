9fd06f3984ef3fce4f43383a38c982f6633f0f6b
Rahul NS <rahul9380767545@gmail.com>
2025-03-17 17:11:59 +0530

    feat: Add 'mark favorite cities' option

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from './components/layout';
 import { ThemeProvider } from './context/theme-provider';
-import WeatherDashboard from './pages/weather-dashboard';
 import CityPage from './pages/city-page';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { WeatherDashboard } from './pages/weather-dashboard';
 
 const queryClient = new QueryClient({
   defaultOptions: {
